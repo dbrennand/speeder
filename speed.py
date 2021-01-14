864f1ef3b90cfb933b62c55cef5d38fa790b21ed
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-01-14 17:30:00 +0000

    Push changes.

--- speed.py
+++ speed.py
@@ -25,49 +25,47 @@ with influxdb.InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT, username=IN
         ## Check if SPEEDTEST_SERVER_ID has been provided, if so, use the server ID
         if SPEEDTEST_SERVER_ID:
             print(f"Running speedtest with server ID: {SPEEDTEST_SERVER_ID}.")
-            process = subprocess.Popen(["./librespeed", f"--server {SPEEDTEST_SERVER_ID}", "--telemetry-level disabled", "--json"], stdout=subprocess.PIPE)
-        else:
-            print(f"Running speedtest.")
-            process = subprocess.Popen(["./librespeed", "--telemetry-level disabled", "--json"], stdout=subprocess.PIPE)
-        # Wait for speedtest to finish
-        result, err = process.communicate()
-        exit_code = process.wait()
-        # Check if speedtest failed
-        if exit_code not 0:
-            # Speedtest failed
-            print(f"Speedtest failed with exit code: {exit_code}.\nError: {err.decode('utf-8')}")
-        else:
-            # Speedtest succeeded
-            print(f"Speedtest succeeded. Parsing JSON and writing results to InfluxDB database: {INFLUXDB_DB_NAME}.")
-            # Load and parse JSON results
-            json_result = json.loads(result.decode('utf-8'))
-            # Write InfluxDB result
-            json_body = [
-                {
-                    "measurement": "internet_speed",
-                    "tags": {
-                        "server_name": json_result["server"]["name"],
-                        "server_url": json_result["server"]["url"],
-                        "hostname": json_result["client"]["hostname"],
-                        "region": json_result["client"]["region"],
-                        "city": json_result["client"]["city"],
-                        "country": json_result["client"]["country"],
-                        "org": json_result["client"]["org"],
-                        "timezone": json_result["client"]["timezone"]
-                    },
-                    "time": json_result["timestamp"],
-                    "fields": {
-                        "bytes_sent": json_result["bytes_sent"],
-                        "bytes_received": json_result["bytes_received"],
-                        "ping": json_result["ping"],
-                        "jitter": json_result["jitter"],
-                        "upload": json_result["upload"],
-                        "download": json_result["download"]
+            result = subprocess.run(["/librespeed", f"--server {SPEEDTEST_SERVER_ID}", "--telemetry-level disabled", "--json"], capture_output=True)
+            # Check if speedtest failed
+            if result.returncode not 0:
+                # Speedtest failed
+                print(f"Speedtest failed with exit code: {result.returncode}.\nError: {result.stderr.decode('utf-8')}")
+            else:
+                # Speedtest succeeded
+                print(f"Speedtest succeeded. Parsing JSON and writing results to InfluxDB database: {INFLUXDB_DB_NAME}.")
+                # Load and parse JSON results
+                json_result = json.loads(result.stdout.decode("utf-8"))
+                # Write InfluxDB result
+                json_body = [
+                    {
+                        "measurement": "internet_speed",
+                        "tags": {
+                            "server_name": json_result["server"]["name"],
+                            "server_url": json_result["server"]["url"],
+                            "hostname": json_result["client"]["hostname"],
+                            "region": json_result["client"]["region"],
+                            "city": json_result["client"]["city"],
+                            "country": json_result["client"]["country"],
+                            "org": json_result["client"]["org"],
+                            "timezone": json_result["client"]["timezone"]
+                        },
+                        "time": json_result["timestamp"],
+                        "fields": {
+                            "bytes_sent": json_result["bytes_sent"],
+                            "bytes_received": json_result["bytes_received"],
+                            "ping": json_result["ping"],
+                            "jitter": json_result["jitter"],
+                            "upload": json_result["upload"],
+                            "download": json_result["download"]
+                        }
                     }
-                }
-            ]
-            # Write results to InfluxDB
-            print(f"Writing results to InfluxDB database: {json_body}")
-            influx.write_points(json_body)
-        # Sleep on the specified interval
-        time.sleep(SPEEDTEST_INTERVAL)
+                ]
+                # Write results to InfluxDB
+                print(f"Writing results to InfluxDB database: {json_body}")
+                influx.write_points(json_body)
+            # Sleep on the specified interval
+            time.sleep(SPEEDTEST_INTERVAL)
+        else:
+            print("Provide a server ID. List of servers below.")
+            servers = subprocess.run(["/librespeed", "--list"], capture_output=True)
+            print(result.stdout.decode("utf-8"))

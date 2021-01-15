37d290d1becce633e2c3292778a2948b17f1a393
dbrennand <52419383+dbrennand@users.noreply.github.com>
2021-01-15 10:24:49 +0000

    Format using black.

--- librespeed-grafana.py
+++ librespeed-grafana.py
@@ -11,7 +11,7 @@ logger.debug(f"Starting librespeed-grafana version: {__version__}.")
 
 # Retrieve environment variables, if not found, use defaults
 ## LibreSpeed CLI environment variables
-SPEEDTEST_INTERVAL = int(os.environ.get("SPEEDTEST_INTERVAL", 1800)) # 30 minutes
+SPEEDTEST_INTERVAL = int(os.environ.get("SPEEDTEST_INTERVAL", 1800))  # 30 minutes
 SPEEDTEST_SERVER_ID = str(os.environ.get("SPEEDTEST_SERVER_ID", None))
 ## InfluxDB environment variables
 INFLUXDB_HOST = os.environ.get("INFLUXDB_HOST", "influxdb")
@@ -22,25 +22,51 @@ INFLUXDB_DB = os.environ.get("INFLUXDB_DB", "internet_speed")
 
 # Check if SPEEDTEST_SERVER_ID environment variable has not been provided
 if not SPEEDTEST_SERVER_ID:
-    logger.debug("SPEEDTEST_SERVER_ID environment variable has no server ID. Choose from the list below and set the environment variable.")
+    logger.debug(
+        "SPEEDTEST_SERVER_ID environment variable has no server ID. Choose from the list below and set the environment variable."
+    )
     servers = subprocess.run(["/librespeed", "--list"], capture_output=True, text=True)
     logger.debug(servers.stdout)
     exit()
 
 # Connect to InfluxDB
-logger.debug(f"Connecting to InfluxDB using host: {INFLUXDB_HOST}, port: {INFLUXDB_PORT}, username: {INFLUXDB_USER}, database name: {INFLUXDB_DB}.")
-influx = influxdb.InfluxDBClient(host=INFLUXDB_HOST, port=INFLUXDB_PORT, username=INFLUXDB_USER, password=INFLUXDB_USER_PASSWORD, database=INFLUXDB_DB, retries=0)
+logger.debug(
+    f"Connecting to InfluxDB using host: {INFLUXDB_HOST}, port: {INFLUXDB_PORT}, username: {INFLUXDB_USER}, database name: {INFLUXDB_DB}."
+)
+influx = influxdb.InfluxDBClient(
+    host=INFLUXDB_HOST,
+    port=INFLUXDB_PORT,
+    username=INFLUXDB_USER,
+    password=INFLUXDB_USER_PASSWORD,
+    database=INFLUXDB_DB,
+    retries=0,
+)
 
 # Run the speedtest using the LibreSpeed CLI on an interval
 while True:
     # Run the speedtest using the LibreSpeed CLI
-    logger.debug(f"Running speedtest with server ID: {SPEEDTEST_SERVER_ID} and telemetry disabled.")
-    result = subprocess.run(["/librespeed", "--server", SPEEDTEST_SERVER_ID, "--telemetry-level", "disabled", "--json"], capture_output=True, text=True)
+    logger.debug(
+        f"Running speedtest with server ID: {SPEEDTEST_SERVER_ID} and telemetry disabled."
+    )
+    result = subprocess.run(
+        [
+            "/librespeed",
+            "--server",
+            SPEEDTEST_SERVER_ID,
+            "--telemetry-level",
+            "disabled",
+            "--json",
+        ],
+        capture_output=True,
+        text=True,
+    )
     # Check if speedtest failed
     if result.returncode != 0:
         # Speedtest failed
         # CLI errors go to stdout
-        logger.debug(f"Speedtest failed with exit code: {result.returncode}.\nError: {result.stdout}")
+        logger.debug(
+            f"Speedtest failed with exit code: {result.returncode}.\nError: {result.stdout}"
+        )
     else:
         # Speedtest succeeded
         logger.debug(f"Speedtest succeeded. Parsing JSON results.")
@@ -59,7 +85,7 @@ while True:
                     "city": json_result["client"]["city"],
                     "country": json_result["client"]["country"],
                     "org": json_result["client"]["org"],
-                    "timezone": json_result["client"]["timezone"]
+                    "timezone": json_result["client"]["timezone"],
                 },
                 "time": json_result["timestamp"],
                 "fields": {
@@ -68,12 +94,14 @@ while True:
                     "ping": float(json_result["ping"]),
                     "jitter": float(json_result["jitter"]),
                     "upload": float(json_result["upload"]),
-                    "download": float(json_result["download"])
-                }
+                    "download": float(json_result["download"]),
+                },
             }
         ]
         # Write results to InfluxDB
-        logger.debug(f"Writing results to InfluxDB database: {INFLUXDB_DB}.\nResults: {json_body}")
+        logger.debug(
+            f"Writing results to InfluxDB database: {INFLUXDB_DB}.\nResults: {json_body}"
+        )
         influx.write_points(json_body)
     # Sleep on the specified interval
     time.sleep(SPEEDTEST_INTERVAL)
